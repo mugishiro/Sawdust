451bdce19155d21bdf5ff59d998fb7c80a98bbf8
mugishiro <force-one0624@hotmail.co.jp>
2019-07-31 10:51:50 +0000

    implment tweet and timeline

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,28 @@
 class HomeController < ApplicationController
+  before_action :set_twitter_client, only: [:timeline, :tweet]
+  
   def index
   end
+  
+  def timeline
+    @friends = @client.friends(current_user).take(20)
+    @timeline = @client.home_timeline(include_entities: true)
+  end
+  
+  def tweet
+    @client.update(params[:text])
+    redirect_to home_path
+  end
+  
+  private
+  
+  def set_twitter_client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV["CONSUMER_KEY"]
+      config.consumer_secret = ENV["CONSUMER_SECRET"]
+      config.access_token = current_user.access_token
+      config.access_token_secret = current_user.access_secret
+    end
+  end
+
 end
